19bfe1e9b38d0e3f7c98d29a89446f4a309aa87f
Maksym Kovalenko <m.v.kovalenko4@gmail.com>
2023-02-22 01:32:45 +0200

    addsome fix

--- API/common/upload/upload.controller.js
+++ API/common/upload/upload.controller.js
@@ -1,7 +1,19 @@
-function uploadSingleImage(req, res) {
-  const file = req.file;
-  console.log(file, "file");
-  res.send("file", file);
-}
+const fs = require("fs");
+const util = require("util");
+const unlinkFile = util.promisify(fs.unlink);
 
-module.exports = { uploadSingleImage };
+const uploadFile = async (req, res) => {
+  const response = await uploadFile(req.file);
+  await unlinkFile(req.file.path);
+  res.status(200).send({ response });
+};
+
+const getFile = async (req, res) => {
+  const key = req.params.key;
+};
+
+const deleteFile = async (req, res) => {
+  const id = req.params.id;
+};
+
+module.exports = { uploadFile, deleteFile };
--- API/common/upload/upload.router.js
+++ API/common/upload/upload.router.js
@@ -1,25 +1,15 @@
+require("dotenv").config();
 const { Router } = require("express");
-const fs = require("fs");
-const util = require("util");
-const unlinkFile = util.promisify(fs.unlink);
-const { uploadSingleImage } = require("./upload.controller");
-
+const { uploadFile } = require("./upload.controller");
 const asyncWrapper = require("../../../utils/asyncWrapper");
-
 const uploadRouter = Router();
-
 const upload = require("./multer");
-const { uploadFile } = require("../../services/s3");
+const { uploadFile, deleteFile } = require("../../services/s3");
 uploadRouter.post(
   "/upload-single",
   upload.single("file"),
-  async (req, res) => {
-    console.log(req, "req");
-    const response = await uploadFile(req.file);
-    await unlinkFile(req.file.path);
-    res.status(200).send({ response });
-  }
-  // asyncWrapper(uploadSingleImage)
+  asyncWrapper(uploadFile)
 );
+uploadRouter.delete("/delete-single/:id", asyncWrapper(deleteFile));
 
 module.exports = uploadRouter;
